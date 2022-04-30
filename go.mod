5ba4dbf0199928988b41e465b4f7fb84699a0ce4
peaceli <peaceli@tencent.com>
2022-04-30 17:27:34 +0800

    add register

--- go.mod
+++ go.mod
@@ -4,7 +4,9 @@ go 1.12
 
 require (
 	github.com/apex/gateway v1.1.2
-	github.com/aws/aws-lambda-go v1.17.0
+	github.com/aws/aws-sdk-go-v2 v1.16.3
+	github.com/aws/aws-sdk-go-v2/config v1.15.4
+	github.com/aws/aws-sdk-go-v2/service/dynamodb v1.15.4
 	github.com/gin-gonic/gin v1.7.7
 	github.com/stretchr/testify v1.6.1
 )
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/apex/gateway"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/gin-gonic/gin"
 )
 
@@ -21,6 +27,34 @@ func welcomeHandler(c *gin.Context) {
 func rootHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"text": "Welcome to gin lambda server.",
+	})
+}
+
+func registerHandle(c *gin.Context) {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), func(o *config.LoadOptions) error {
+		o.Region = "us-east-1"
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+	svc := dynamodb.NewFromConfig(cfg)
+	out, err := svc.PutItem(context.TODO(), &dynamodb.PutItemInput{
+		TableName: aws.String("user"),
+		Item: map[string]types.AttributeValue{
+			"id":    &types.AttributeValueMemberS{Value: "12346"},
+			"name":  &types.AttributeValueMemberS{Value: "John Doe"},
+			"email": &types.AttributeValueMemberS{Value: "john@doe.io"},
+		},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(out.Attributes)
+	c.JSON(http.StatusOK, gin.H{
+		"text": "ok",
 	})
 }
 
